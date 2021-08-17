07bfe99e911d39c0173458e4e9b94a3b5e3a8083
kelwinap <kelwinarthur8@gmail.com>
2021-08-17 10:43:43 -0300

    ensure token generator returns null if jwt returns null

---
+++ src/utils/helpers/token-generator.spec.js
@@ -0,0 +1,20 @@
+
+class TokenGenerator {
+  async generate (id) {
+    return null
+  }
+}
+
+describe('Token Generator', () => {
+  test('should return null if JWT returns null', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBeNull()
+  })
+
+  test('should return null if JWT returns null', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBeNull()
+  })
+})
