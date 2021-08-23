01eac8a4ecb5ea7cf02fff3ca094b13771af6091
kelwinap <kelwinarthur8@gmail.com>
2021-08-23 14:51:50 -0300

    return xml when forcerd

--- src/main/config/middlewares/content-type.test.js
+++ src/main/config/middlewares/content-type.test.js
@@ -9,4 +9,13 @@ describe('Content Type', () => {
 
     await request(app).get('/test_content_type').expect('content-type', /json/)
   })
+
+  test('should return xml content type if forced', async () => {
+    app.get('/test_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app).get('/test_xml').expect('content-type', /xml/)
+  })
 })
